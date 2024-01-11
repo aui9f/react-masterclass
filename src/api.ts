d08eaca28b035be421d499c80e35e55c2e715037
aui9f <aui9f@naver.com>
2024-01-11 12:40:41 +0900

    Coin Api 수정

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,16 @@
-export function fetchCoins(){
-    return fetch('https://api.coinpaprika.com/v1/coins').then(res=>{
-        return res.json();
-    })
+const BASE_URL = "https://api.coinpaprika.com/v1";
+
+export function fetchCoins() {
+  return fetch(`${BASE_URL}/coins`).then((res) => {
+    return res.json();
+  });
+}
+export function fetchCoin(coinId: string) {
+  return fetch(`${BASE_URL}/coins/${coinId}`).then((res) => {
+    return res.json();
+  });
+}
+
+export function fetchTickers(coinId: string) {
+  return fetch(`${BASE_URL}/tickers/${coinId}`).then((res) => res.json());
 }
