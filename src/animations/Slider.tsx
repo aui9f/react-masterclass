7f03bd4b8f8e39a0d16fd6b060fd4956cb5fde88
aui9f <aui9f@naver.com>
2024-02-28 16:38:45 +0900

    슬라이더 (근데 이전으로 이동할때 방향이 이상함..

--- src/animations/Slider.tsx
+++ src/animations/Slider.tsx
@@ -1,11 +1,17 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import styled from "styled-components";
-
+const Container = styled.div`
+  isplay: flex;
+  flex-direction: column;
+`
 const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex: 1;
+  min-height: 160px;
+  position: relative;
 `;
 
 const Box = styled(motion.div)`
@@ -22,48 +28,65 @@ const Box = styled(motion.div)`
 `;
 
 const vBox = {
-  invisible: {
-    x: 500,
-    opacity: 0,
-    scale: 0
-  },
-  visible: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 1,
-    },
-  },
-  exit: {
-    x: -500,
+  entry: (isBack: boolean) => ({
+    x: isBack ? -500 : 500,
     opacity: 0,
     scale: 0,
-    rotateX: 180,
-    transition: {
-      duration: 1
-    }
-  }
+    }),
+    center: {
+      x: 0,
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 1,
+      },
+    },
+    exit: (isBack:boolean) => ({
+      x: isBack ? 300 : -300,
+      opacity: 0,
+      scale: 0,
+      transition: { duration: 1 }
+    }),
 }
 
 function Slider(){
   const [selected, setSelected] = useState(1);
-  const onNext = () => setSelected(prev=>prev===10 ? 10 : prev+1);
-  const onPrev = () => {setSelected(prev=>prev===1 ? 1 : prev-1);}
-  return <>
+  const [isBack, setIsBack] = useState(false);
+  
+  const onNext = async() => {
+    await setIsBack(false);
+    
+    setSelected(prev => (prev === 10 ? 10 : prev + 1));
+  }
+
+  const onPrev = async() => {
+    await setIsBack(true);
+    setSelected(prev => (prev === 1 ? 1 : prev - 1));
+  }
+
+
+  return <Container>
   <Wrapper>
-    <AnimatePresence>
-      {[1,2,3,4,5,6,7,8,9,10].map(x=>(
-        selected===x?<Box variants={vBox} initial="invisible" animate="visible" exit="exit" key={x}>{x}</Box>:null
-      ))}
+    <AnimatePresence custom={isBack}>
+    <Box  variants={vBox} 
+      initial="entry" 
+      animate="center" 
+      exit="exit"
+       key={selected}>{selected}</Box>
     </AnimatePresence>
 
 
   </Wrapper>
       
-      <button onClick={onPrev}>이전</button>
+      <button onClick={onPrev}>이전(isBack: {isBack?'TRUE':'FALSE'})</button>
       <button onClick={onNext}>다음</button>
-  </>
+  </Container>
 }
 
 export default Slider
+
+
+/**
+ * Custom Property==
+ * exitBeforeEnter(지원하지않음)-> mode="wait" -- exit가 완료된 이후 실행
+ */
